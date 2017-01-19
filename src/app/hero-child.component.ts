f6838b03892d0a44dc1b3ca48714c56fa96ac78e
Jaya Krishna <jaya.pulipati@ptgindia.com>
2017-01-19 18:21:25 +0530

    renamed master name and added lifecycle methods of component.

--- src/app/hero-child.component.ts
+++ src/app/hero-child.component.ts
@@ -1,14 +1,26 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, OnChanges} from "@angular/core";
 import {Hero} from "./shared/services/hero.service";
 
 @Component({
   selector: 'hero-child',
   template: `
+    <ng-content></ng-content>
     <h3>{{hero.name}} says:</h3>
-    <p>I, {{hero.name}}, am at your service.</p>
+    <p>I, {{hero.name}}, am at your service. {{masterName}}</p>
+    <input type="text" bindon-ngModel="master" />
+    <input type="text" bindon-ngModel="hero.name" />
+    <span (click)="changeMasterName()">Click</span>
   `
 })
-export class HeroChildComponent {
+export class HeroChildComponent implements OnChanges{
   @Input() hero;
-  @Input('master') masterName: string;
+  @Input() master: string;
+
+  ngOnChanges() {
+    console.log('child ngOnChanges');
+  }
+
+  changeMasterName() {
+    this.master = 'Raju';
+  }
 }
--- src/app/hero-parent.component.ts
+++ src/app/hero-parent.component.ts
@@ -1,6 +1,10 @@
-import {Component, OnInit} from "@angular/core";
+import {
+  Component, OnInit, AfterContentChecked, ContentChild, AfterContentInit, DoCheck,
+  OnChanges
+} from "@angular/core";
 import {DetailsService} from "./shared/services/details.service";
 import {HeroService, Hero} from "./shared/services/hero.service";
+import {HeroChildComponent} from "./hero-child.component";
 
 @Component({
   selector: 'hero-parent',
@@ -8,12 +12,13 @@ import {HeroService, Hero} from "./shared/services/hero.service";
     <h2>{{master}} controls {{heroes.length}} heroes</h2>
     <hero-child *ngFor="let hero of heroes"
         [hero]="hero"
+        [master]="master"
       >
     </hero-child>
   `
 })
 
-export class HeroPatentComponent implements OnInit{
+export class HeroPatentComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked{
   heroes: Hero[];
   master: string = 'Master';
   constructor(private details:DetailsService, private heroService: HeroService){}
@@ -21,5 +26,24 @@ export class HeroPatentComponent implements OnInit{
   ngOnInit() {
     // this.heroes = this.details.getDetails()
     this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnChanges() {
+    console.log('parent ngOnChanges');
+  }
+
+  ngDoCheck() {
+    console.log('changes were done', this);
+  }
+
+  @ContentChild(HeroChildComponent) contentChild: HeroChildComponent;
+
+  ngAfterContentInit() {
+    console.log('child component was initialized');
+  }
+
+  ngAfterContentChecked() {
+    console.log('child component was checked');
+    console.log('sdfsdf', this.master);
   }
 }
